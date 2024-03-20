b6a547eebe177ff07535075d73b2292ed7f58ea2
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-03-20 12:43:27 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <=400) {
+    return 'This one is on me!'
+  }
+  else if (feet <=2000) {
+    return 'That will be twenty bucks.'
+  }
+     
+  else if (feet <=2001) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet >2500) {
+    return 'No can do.'
+  }
+    
+    }
+
+function ternaryCheckCity(_city) {
+return _city === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(_tip){
   // Write your code here!
+  switch (_tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      default:
+        return 'Bye.'
+  }
 }
